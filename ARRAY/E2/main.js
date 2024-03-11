81868bec9b0b73c9e014481a8239cf7af3e74044
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-11 21:03:58 +0100

    comiendo e2

---
+++ ARRAY/E2/main.js
@@ -0,0 +1,15 @@
+const listadoPasajeros =[
+    "Jack Dawson",
+    "Rose DeWitt Bukater",
+    "Thomas Andrews",
+    "Margaret Brown",
+    "John Jacob Astor IV",
+    "Molly Brown",
+    "Captain Edward Smith",
+    "J. Bruce Ismay",
+    "Benjamin Guggenheim",
+    "Wallace Hartley",
+    "Albert Vandellos",
+    "Margareth Leeds"
+];
+console.log("Listado de animales: " + listadoPasajeros);
