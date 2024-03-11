d421db30a0fd325f4e91674c1edafdb650f3a220
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-11 22:03:23 +0100

    terminado sin botones

--- ARRAY/E2/main.js
+++ ARRAY/E2/main.js
@@ -12,30 +12,46 @@ const listadoPasajeros =[
     "Albert Vandellos",
     "Margareth Leeds"
 ];
-console.log("Listado de animales: " + listadoPasajeros);
+console.log("Listado de pasajeros: " + listadoPasajeros);
 
 //mala espina, se bajand 2
 listadoPasajeros.pop();
 listadoPasajeros.pop();
 
-console.log("Listado de animales: " + listadoPasajeros);
+console.log("Listado de pasajeros: " + listadoPasajeros);
 
 //se suman dos pasajeros nuevos
 
 listadoPasajeros.push("Lucile Carter",
 "Charles Lightoller");
-console.log("Listado de animales: " + listadoPasajeros);
+console.log("Listado de pasajeros: " + listadoPasajeros);
 
 //cambia el segundo pasajero
 
 listadoPasajeros[1] = "Rose DeWitt Bukater";
 
-console.log("Listado de animales: " + listadoPasajeros);
+console.log("Listado de pasajeros: " + listadoPasajeros);
 
-console.log("Eston son pasajeros del dia de hoy: ");
+
+console.log("Estos son pasajeros del dia de hoy: ");
 
 for (let i=0; i<listadoPasajeros.length; i++){
     
     console.log(listadoPasajeros[i])
 }
 
+console.log (listadoPasajeros.length) ;
+//Imprimir en pantalla
+
+function finalList() {
+    let pasajero = "<ul>"; 
+
+    for (let i = 0; i < listadoPasajeros.length; i++) {
+        pasajero += "<li>" + listadoPasajeros[i] + "</li>"; 
+    }
+
+    pasajero += "</ul>"; 
+    document.getElementById("finalList").innerHTML = pasajero; 
+}
+
+finalList();
