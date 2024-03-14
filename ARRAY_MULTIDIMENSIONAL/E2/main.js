cb512fcf14b99bb008396a4ca49a6eca6adc6ae3
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-14 18:28:29 +0100

    agregado el LET!

--- ARRAY_MULTIDIMENSIONAL/E2/main.js
+++ ARRAY_MULTIDIMENSIONAL/E2/main.js
@@ -1,7 +1,7 @@
 const medidas = [[175, 134], 165, [80, 187, [73, 26], 92]];
 const nombres = [["Kitty", "Toby", "Didi"], ["Pedro", "Jose", "Roberto"], ["Carla", "Rosa", "Julieta"]];
 
-medidas2 = medidas.flat(Infinity);
+let medidas2 = medidas.flat(Infinity);
 console.log(medidas2);
 console.log("Si hacemos un FLAT, el Index del 26 es: " + medidas2.indexOf(26));
 
@@ -12,7 +12,7 @@ console.log(nombres[2][1]);
 
 console.log("Leer mascotas:");
 console.log(nombres[0]);
-nombres2 = nombres[0];
+let nombres2 = nombres[0];
 for (let i=nombres2; i<=nombres2; i++){
     console.log("los nombres de mascotas son: " + i )};
 
